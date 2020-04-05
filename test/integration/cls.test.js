01461fb3f293f80a5501603dd24f5c9b2f44b52e
lilangxiong <1624244265@qq.com>
2020-04-05 20:58:36 +0800

    commit first

---
+++ test/integration/cls.test.js
@@ -0,0 +1,156 @@
+'use strict';
+
+const chai      = require('chai'),
+  expect    = chai.expect,
+  Support   = require('./support'),
+  Sequelize = Support.Sequelize,
+  Promise   = Sequelize.Promise,
+  cls       = require('cls-hooked'),
+  current = Support.sequelize;
+
+if (current.dialect.supports.transactions) {
+  describe(Support.getTestDialectTeaser('CLS (Async hooks)'), () => {
+    before(() => {
+      current.constructor.useCLS(cls.createNamespace('sequelize'));
+    });
+
+    after(() => {
+      cls.destroyNamespace('sequelize');
+      delete Sequelize._cls;
+    });
+
+    beforeEach(async function() {
+      this.sequelize = await Support.prepareTransactionTest(this.sequelize);
+      this.ns = cls.getNamespace('sequelize');
+      this.User = this.sequelize.define('user', {
+        name: Sequelize.STRING
+      });
+      await this.sequelize.sync({ force: true });
+    });
+
+    describe('context', () => {
+      it('does not use continuation storage on manually managed transactions', function() {
+        return Sequelize._clsRun(async () => {
+          const transaction = await this.sequelize.transaction();
+          expect(this.ns.get('transaction')).not.to.be.ok;
+          await transaction.rollback();
+        });
+      });
+
+      it('supports several concurrent transactions', async function() {
+        let t1id, t2id;
+        await Promise.all([
+          this.sequelize.transaction(async () => {
+            t1id = this.ns.get('transaction').id;
+          }),
+          this.sequelize.transaction(async () => {
+            t2id = this.ns.get('transaction').id;
+          })
+        ]);
+        expect(t1id).to.be.ok;
+        expect(t2id).to.be.ok;
+        expect(t1id).not.to.equal(t2id);
+      });
+
+      it('supports nested promise chains', function() {
+        return this.sequelize.transaction(() => {
+          const tid = this.ns.get('transaction').id;
+
+          return this.User.findAll().then(() => {
+            expect(this.ns.get('transaction').id).to.be.ok;
+            expect(this.ns.get('transaction').id).to.equal(tid);
+          });
+        });
+      });
+
+      it('does not leak variables to the outer scope', function() {
+        // This is a little tricky. We want to check the values in the outer scope, when the transaction has been successfully set up, but before it has been comitted.
+        // We can't just call another function from inside that transaction, since that would transfer the context to that function - exactly what we are trying to prevent;
+
+        let transactionSetup = false,
+          transactionEnded = false;
+
+        this.sequelize.transaction(() => {
+          transactionSetup = true;
+
+          return Promise.delay(500).then(() => {
+            expect(this.ns.get('transaction')).to.be.ok;
+            transactionEnded = true;
+          });
+        });
+
+        return new Promise(resolve => {
+          // Wait for the transaction to be setup
+          const interval = setInterval(() => {
+            if (transactionSetup) {
+              clearInterval(interval);
+              resolve();
+            }
+          }, 200);
+        }).then(() => {
+          expect(transactionEnded).not.to.be.ok;
+
+          expect(this.ns.get('transaction')).not.to.be.ok;
+
+          // Just to make sure it didn't change between our last check and the assertion
+          expect(transactionEnded).not.to.be.ok;
+        });
+      });
+
+      it('does not leak variables to the following promise chain', function() {
+        return this.sequelize.transaction(() => {
+          return Promise.resolve();
+        }).then(() => {
+          expect(this.ns.get('transaction')).not.to.be.ok;
+        });
+      });
+
+      it('does not leak outside findOrCreate', function() {
+        return this.User.findOrCreate({
+          where: {
+            name: 'Kafka'
+          },
+          logging(sql) {
+            if (/default/.test(sql)) {
+              throw new Error('The transaction was not properly assigned');
+            }
+          }
+        }).then(() => {
+          return this.User.findAll();
+        });
+      });
+    });
+
+    describe('sequelize.query integration', () => {
+      it('automagically uses the transaction in all calls', function() {
+        return this.sequelize.transaction(() => {
+          return this.User.create({ name: 'bob' }).then(() => {
+            return Promise.all([
+              expect(this.User.findAll({ transaction: null })).to.eventually.have.length(0),
+              expect(this.User.findAll({})).to.eventually.have.length(1)
+            ]);
+          });
+        });
+      });
+
+      it('automagically uses the transaction in all calls with async/await', function() {
+        return this.sequelize.transaction(async () => {
+          await this.User.create({ name: 'bob' });
+          expect(await this.User.findAll({ transaction: null })).to.have.length(0);
+          expect(await this.User.findAll({})).to.have.length(1);
+        });
+      });
+    });
+
+    it('CLS namespace is stored in Sequelize._cls', function() {
+      expect(Sequelize._cls).to.equal(this.ns);
+    });
+
+    it('promises returned by sequelize.query are correctly patched', function() {
+      return this.sequelize.transaction(t =>
+        this.sequelize.query('select 1', { type: Sequelize.QueryTypes.SELECT })
+          .then(() => expect(this.ns.get('transaction')).to.equal(t))
+      );
+    });
+  });
+}
---
+++ test/integration/dialects/sqlite/connection-manager.test.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const chai = require('chai');
+const jetpack = require('fs-jetpack').cwd(__dirname);
+const expect = chai.expect;
+const Support = require('../../support');
+const dialect = Support.getTestDialect();
+const DataTypes = require('../../../../lib/data-types');
+
+const fileName = `${Math.random()}_test.sqlite`;
+const directoryName = `${Math.random()}_test_directory`;
+const nestedFileName = jetpack.path(directoryName, 'subdirectory', 'test.sqlite');
+
+if (dialect === 'sqlite') {
+  describe('[SQLITE Specific] Connection Manager', () => {
+    after(() => {
+      jetpack.remove(fileName);
+      jetpack.remove(directoryName);
+    });
+
+    it('close connection and remove journal and wal files', function() {
+      const sequelize = Support.createSequelizeInstance({
+        storage: jetpack.path(fileName)
+      });
+      const User = sequelize.define('User', { username: DataTypes.STRING });
+
+      return User
+        .sync({ force: true })
+        .then(() => sequelize.query('PRAGMA journal_mode = WAL'))
+        .then(() => User.create({ username: 'user1' }))
+        .then(() => {
+          return sequelize.transaction(transaction => {
+            return User.create({ username: 'user2' }, { transaction });
+          });
+        })
+        .then(() => {
+          expect(jetpack.exists(fileName)).to.be.equal('file');
+          expect(jetpack.exists(`${fileName}-shm`), 'shm file should exists').to.be.equal('file');
+          expect(jetpack.exists(`${fileName}-wal`), 'wal file should exists').to.be.equal('file');
+
+          return sequelize.close();
+        })
+        .then(() => {
+          expect(jetpack.exists(fileName)).to.be.equal('file');
+          expect(jetpack.exists(`${fileName}-shm`), 'shm file exists').to.be.false;
+          expect(jetpack.exists(`${fileName}-wal`), 'wal file exists').to.be.false;
+
+          return this.sequelize.query('PRAGMA journal_mode = DELETE');
+        });
+    });
+
+    it('automatic path provision for `options.storage`', () => {
+      return Support.createSequelizeInstance({ storage: nestedFileName })
+        .define('User', { username: DataTypes.STRING })
+        .sync({ force: true }).then(() => {
+          expect(jetpack.exists(nestedFileName)).to.be.equal('file');
+        });
+    });
+  });
+}
